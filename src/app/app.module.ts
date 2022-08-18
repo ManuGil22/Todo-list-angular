c364a3950e3ea7c702550a99dded9a2d5c88fe00
Manuel Gil <manuelgilmoltrasio@gmail.com>
2022-08-18 10:46:18 -0300

    new-task section added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TextInputComponent } from './components/text-input/text-input.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BtnComponent } from './components/btn/btn.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextInputComponent,
+    HeaderComponent,
+    BtnComponent
   ],
   imports: [
     BrowserModule
