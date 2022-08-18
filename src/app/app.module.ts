1b382cc729024c072a2c1d779812229ea7fba666
Manuel Gil <manuelgilmoltrasio@gmail.com>
2022-08-18 17:04:06 -0300

    task-list added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,21 @@ import { AppComponent } from './app.component';
 import { TextInputComponent } from './components/text-input/text-input.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BtnComponent } from './components/btn/btn.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskComponent } from './components/task/task.component';
+import { CheckboxComponent } from './components/checkbox/checkbox.component';
+import { DeleteBtnComponent } from './components/delete-btn/delete-btn.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TextInputComponent,
     HeaderComponent,
-    BtnComponent
+    BtnComponent,
+    TaskListComponent,
+    TaskComponent,
+    CheckboxComponent,
+    DeleteBtnComponent,
   ],
   imports: [
     BrowserModule
